eeff9f3b8d009b974ee1f9ea3a99a3d28f8056f6
shipatulhasan <shipatulhasan328@gmail.com>
2023-06-13 22:55:34 +0600

    added layout

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Toaster } from "react-hot-toast";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
+import "./App";
+
+function App() {
+  return (
+    <div>
+      <h2 className="text-base">Hello World</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import AuthProvider from "./AuthProvider/AuthProvider";
+import "./index.css";
+import "react-phone-number-input/style.css";
+axios.defaults.baseURL = "https://e-commerce-repliq-server.vercel.app";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
